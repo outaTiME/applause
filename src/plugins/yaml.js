a1545184c13a476bb60885287c8c3fd20fd80ed6
Maximiliano Fierro <elmasse@gmail.com>
2014-03-22 11:54:37 -0300

    Reindent to 2 spaces

--- src/plugins/yaml.js
+++ src/plugins/yaml.js
@@ -1,30 +1,30 @@
 'use strict';
 
 var yaml     = require('js-yaml'),
-    cocktail = require('cocktail');
+cocktail = require('cocktail');
 
 cocktail.mix({
-    '@exports' : module,
-    '@as'      : 'class',
+  '@exports' : module,
+  '@as'      : 'class',
 
-    name       : 'yaml',
-    priority   : 10,
+  name       : 'yaml',
+  priority   : 10,
+  
+  match: function (pattern, opts) {
+    var yaml = pattern.yaml;
+    var match = typeof yaml !== 'undefined';
     
-    match: function (pattern, opts) {
-        var yaml = pattern.yaml;
-        var match = typeof yaml !== 'undefined';
-        
-        return match;
-    },
-    
-    transform: function (pattern, opts, done) {
-        try {
-            done({
-                json: yaml.safeLoad(pattern.yaml)
-            });
-        } catch (e) {
-            done(e);
-        }
+    return match;
+  },
+  
+  transform: function (pattern, opts, done) {
+    try {
+      done({
+        json: yaml.safeLoad(pattern.yaml)
+      });
+    } catch (e) {
+      done(e);
     }
+  }
 
 });
