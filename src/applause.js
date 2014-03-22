668d68b81c07e5ca7e746760f277302a21b1d414
Maximiliano Fierro <elmasse@gmail.com>
2014-03-22 11:53:54 -0300

    Remove getPlugins method. Passing list as param to execute.

--- src/applause.js
+++ src/applause.js
@@ -1,218 +1,226 @@
 'use strict';
 
 var cocktail     = require('cocktail'),
-    Evented      = require('cocktail-annotation-evented'),
-    _            = require('lodash'),
-    Pluggable    = require('./pluggable');
+Evented      = require('cocktail-annotation-evented'),
+_            = require('lodash'),
+Pluggable    = require('./pluggable');
 
 cocktail.use(Evented);
 
 cocktail.mix({
-    '@exports': module,
-    '@as'     : 'class',
+  '@exports': module,
+  '@as'     : 'class',
 
-    '@evented': true,
+  '@evented': true,
 
-    '@static' : {
+  '@static' : {
 
-        create : function (options) {
-            var Applause = this;
-            return new Applause(options);
-        }
+    create : function (options) {
+      var Applause = this;
+      return new Applause(options);
+    }
+  },
+
+  '@properties' : {
+    options: null
+  },
+
+  '@traits': [Pluggable],
+
+  _plugins : null,
+
+  constructor: function (options) {
+    var me = this,
+    opts;
+
+    opts = _.defaults(options, {
+      patterns: [],
+      prefix: options.usePrefix === false ? '': '@@',
+      usePrefix: true,
+      preservePrefix: false,
+      delimiter: '.',
+      preserveOrder: false
+    });
+
+    this.setOptions(opts);
+
+    this._plugins = [];
+
+    this.initializePlugins();
+  },
+
+    /**
+     * @public
+     * @method addPlugin 
+     * @param plugin {Object} the plugin to be added to the list. It must be an Object with 
+     * match() and transform() methods.
+     */
+     addPlugin: function (plugin) {
+      this._plugins.push(plugin);
     },
 
-    '@properties' : {
-        options: null
-    },
-
-    '@traits': [Pluggable],
-
-    _plugins : null,
-
-    constructor: function (options) {
-        var me = this,
-            opts;
-
-        opts = _.defaults(options, {
-            patterns: [],
-            prefix: options.usePrefix === false ? '': '@@',
-            usePrefix: true,
-            preservePrefix: false,
-            delimiter: '.',
-            preserveOrder: false
-        });
-
-        this.setOptions(opts);
-
-        this._plugins = [];
-
-        this.initializePlugins();
-    },
-
-    getPlugins: function () {
-        return this._plugins;
-    },
-
-    addPlugin: function (plugin) {
-        this._plugins.push(plugin);
-    },
-
-    replace : function (contents, process) {
-        var me = this;
-        
-        try {
+    /**
+     * @public
+     * @method replace
+     * @param contents
+     * @param process
+     */
+     replace : function (contents, process) {
+      var me = this;
+      
+      try {
             // prepare patterns
             var patterns = me._prepare();
             // by default file not updated
             var updated = false;
             // iterate over each pattern and make replacement
             patterns.forEach(function (pattern) {
-                var match = pattern.match;
-                var replacement = pattern.replacement;
+              var match = pattern.match;
+              var replacement = pattern.replacement;
                 // wrap replacement function to add process arguments
                 
                 if (_.isFunction(replacement)) {
-                    replacement = function () {
-                        var args = Array.prototype.slice.call(arguments);
-                        return pattern.replacement.apply(this, args.concat(process || []));
-                    };
+                  replacement = function () {
+                    var args = Array.prototype.slice.call(arguments);
+                    return pattern.replacement.apply(this, args.concat(process || []));
+                  };
                 }
                 updated = updated || contents.match(match);
                 contents = contents.replace(match, replacement);
-            });
+              });
 
             if (!updated) {
-                return false;
+              return false;
             }
             return contents;
-        } catch (e) {
+          } catch (e) {
             me._error(e);
-        }
-    },
+          }
+        },
 
-    _prepare: function() {
-        var me        = this,
-            opts      = me.getOptions(),
-            patterns  =  _.clone(opts.patterns),
-            variables = opts.variables,
-            plugins   = me._plugins;
+        _prepare: function() {
+          var me        = this,
+          opts      = me.getOptions(),
+          patterns  =  _.clone(opts.patterns),
+          variables = opts.variables,
+          plugins   = me._plugins;
 
-        if (!_.isEmpty(variables)) {
+          if (!_.isEmpty(variables)) {
             patterns.push({
-                json: variables
+              json: variables
             });
-        }
+          }
 
         // intercept errors
         for (var i = patterns.length - 1; i >= 0; i -= 1) {
-            var context = {
-                pattern: patterns[i]
-            };
+          var context = {
+            pattern: patterns[i]
+          };
             // process context with each plugin
-            me.executePluginsInContext(context, opts);
+            me.executePluginsInContext(plugins, context, opts);
             // update current pattern
             Array.prototype.splice.apply(patterns, [i, 1].concat(context.pattern));
-        }
-        
-        me._sortPatterns(patterns);
+          }
+          
+          me._sortPatterns(patterns);
 
         // normalize definition
         return me._normalize(patterns);
-    },
+      },
 
-    _sortPatterns: function (patterns) {
+      _sortPatterns: function (patterns) {
         var opts = this.getOptions();
 
         if (opts.preserveOrder !== true) {
             // only sort non regex patterns (prevents replace issues like head, header)
             patterns.sort(function (a, b) {
-                var x = a.match;
-                var y = b.match;
-                
-                if (_.isString(x) && _.isString(y)) {
-                    return y.length - x.length;
-                } else if (_.isString(x)) {
-                    return -1;
-                }
-                return 1;
+              var x = a.match;
+              var y = b.match;
+              
+              if (_.isString(x) && _.isString(y)) {
+                return y.length - x.length;
+              } else if (_.isString(x)) {
+                return -1;
+              }
+              return 1;
             });
-        }
-    },
+          }
+        },
 
 
-    _normalize: function (patterns) {
-        var me = this,
-            opts = this.getOptions();
-  
-        return _.transform(patterns, function (result, pattern) {
+        _normalize: function (patterns) {
+          var me = this,
+          opts = this.getOptions();
+          
+          return _.transform(patterns, function (result, pattern) {
             var match = pattern.match;
             var replacement = pattern.replacement;
             var expression = pattern.expression === true;
-        
+            
             // match check
             if (_.isRegExp(match)) {
-                expression = true;
+              expression = true;
             } else if (_.isString(match)) {
-                if (match.length > 0) {
-                    if (expression === true) {
-                        var index = match.lastIndexOf('/');
-                        if (match[0] === '/' && index > 0) {
-                            try {
-                                match = new RegExp(match.slice(1, index), match.slice(index + 1));
-                            } catch (error) {
-                                me._error(error);
-                                return;
-                            }
-                        } else {
-                            me._error('Invalid expression found for match: ' + match);
-                            return;
-                        }
-                    } else {
+              if (match.length > 0) {
+                if (expression === true) {
+                  var index = match.lastIndexOf('/');
+                  if (match[0] === '/' && index > 0) {
+                    try {
+                      match = new RegExp(match.slice(1, index), match.slice(index + 1));
+                    } catch (error) {
+                      me._error(error);
+                      return;
+                    }
+                  } else {
+                    me._error('Invalid expression found for match: ' + match);
+                    return;
+                  }
+                } else {
                         // old school
                         try {
-                            match = new RegExp(opts.prefix + match, 'g');
+                          match = new RegExp(opts.prefix + match, 'g');
                         } catch (error) {
-                            me._error(error);
-                            return;
+                          me._error(error);
+                          return;
                         }   
-                    }
-                } else {
+                      }
+                    } else {
                     // empty match
                     return;
+                  }
+                } else {
+                  me._error('Unsupported type for match (RegExp or String expected).');
+                  return;
                 }
-            } else {
-                me._error('Unsupported type for match (RegExp or String expected).');
-                return;
-            }
             // replacement check
             if (!_.isFunction(replacement)) {
-                if (!_.isString(replacement)) {
+              if (!_.isString(replacement)) {
                     // transform object to string
                     replacement = JSON.stringify(replacement);
-                } else {
+                  } else {
                     // easy way
                     if (expression === false && opts.preservePrefix === true) {
-                        replacement = opts.prefix + replacement;
+                      replacement = opts.prefix + replacement;
                     }
-                }
-            } else {
+                  }
+                } else {
                 // replace using function return value
-            }
-            return result.push({
+              }
+              return result.push({
                 match: match,
                 replacement: replacement,
                 expression: expression
+              });
             });
-        });
 
-    },
+},
 
-    _error: function(e) {
-      var message = e;
-        if (e instanceof Error) {
-            message = e.message;
-        }
-        this.emit('error', e);    
-    }    
+_error: function(e) {
+  var message = e;
+  if (e instanceof Error) {
+    message = e.message;
+  }
+  this.emit('error', e);    
+}    
 
 });
--- src/pluggable.js
+++ src/pluggable.js
@@ -1,61 +1,60 @@
 'use strict';
 
 var cocktail = require('cocktail'),
-    path     = require('path');
+path     = require('path');
 
 cocktail.mix({
-    '@exports': module,
-    '@as'     : 'class',
+  '@exports': module,
+  '@as'     : 'class',
 
-    '@requires': [
-        'addPlugin',
-        'getPlugins'
-    ],
+  '@requires': [
+  'addPlugin',
+  'getPlugins'
+  ],
 
-    initializePlugins: function () {
-        var me = this,
-            dir = path.join(__dirname, '/plugins');
+  initializePlugins: function () {
+    var me = this,
+    dir = path.join(__dirname, '/plugins');
 
-        require('fs').readdirSync(dir).forEach(function (file) {
-          if (file.match(/.+\.js/g) !== null && file !== 'index.js') {
-            var Plugin = require(path.join(dir, file));
- 
-            me.addPlugin(new Plugin());
-          }
-        });
+    require('fs').readdirSync(dir).forEach(function (file) {
+      if (file.match(/.+\.js/g) !== null && file !== 'index.js') {
+        var Plugin = require(path.join(dir, file));
+        
+        me.addPlugin(new Plugin());
+      }
+    });
 
-    },
+  },
 
-    executePluginsInContext: function (context, opts) {
-        var me = this,
-            plugins = me.getPlugins(),
-            pluginHandler;
+  executePluginsInContext: function (plugins, context, opts) {
+    var me = this,
+    pluginHandler;
 
-        plugins.sort(function(a, b) {
-            return (a.priority || 0) - (b.priority || 0);
-        });
+    plugins.sort(function(a, b) {
+      return (a.priority || 0) - (b.priority || 0);
+    });
 
         // FIXME: handler for plugin management
         pluginHandler = function (context) {
-            return function (plugin) {
-                var pattern = context.pattern;
-                
-                if (plugin.match(pattern, opts) === true) {
-                    plugin.transform(pattern, opts, function (items) {
-                        if (items instanceof Error) {
-                            throw items;
-                        } else {
+          return function (plugin) {
+            var pattern = context.pattern;
+            
+            if (plugin.match(pattern, opts) === true) {
+              plugin.transform(pattern, opts, function (items) {
+                if (items instanceof Error) {
+                  throw items;
+                } else {
                             // store transformed pattern in context
                             context.pattern = items;
-                        }
-                    });
-                } else {
+                          }
+                        });
+            } else {
                     // plugin doesn't apply
-                }
-            };
-        };
+                  }
+                };
+              };
 
-        plugins.forEach(pluginHandler(context));
+              plugins.forEach(pluginHandler(context));
 
-    }
-});
+            }
+          });
